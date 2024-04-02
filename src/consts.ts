fcb09afacee211e128bc628ba39e696526128144
Alberto Trevizo <albertotrevizo@MacBook-Pro-de-Alberto.local>
2024-04-02 13:34:32 -0600

    Little changes and add .cpanel.yml file

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = "GeoCiencia Ciudadana";
+export const SITE_DESCRIPTION =
+  "Creación de un colectivo de ingenieros, academicos, expertos, estudiantes y entusiastas para la divulgación geocientifica";
